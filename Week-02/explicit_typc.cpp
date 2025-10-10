8f7bda374344e446c181b7382129b8234637af53
Ahmed <ahmedhassanpkpkus@gmail.com>
2025-10-10 11:46:48 +0500

    type casting

---
+++ Week-02/explicit_typc.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a = 9.78;
+    int b = (int)a;
+    cout << "Value of b: " << b << endl;
+    return 0;
+}
---
+++ Week-02/implicit_typc.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    float b = a;
+    cout << "Value of b: " << b << endl;
+    return 0;
+}
